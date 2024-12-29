b6daf22cd640aab8fbc881281ba3cda892a6de23
Girish Garg <141021178+Girish-Garg@users.noreply.github.com>
2024-12-29 16:30:30 +0530

    Update commit-tree.js

--- app/git/commands/commit-tree.js
+++ app/git/commands/commit-tree.js
@@ -13,8 +13,8 @@ class commit_tree {
         const commitBuffer = Buffer.concat([
             Buffer.from(`tree ${this.treeSHA}\n`),
             Buffer.from(`parent ${this.commitSHA}\n`),
-            Buffer.from(`author Hemang Choudhary <coehemang@gmail.com> ${Date.now()} +0000\n`),
-            Buffer.from(`committer Hemang Choudhary <coehemang@gmail.com> ${Date.now()} +0000\n\n`),
+            Buffer.from(`author Girish Garg ${Date.now()} +0000\n`),
+            Buffer.from(`committer Girish Garg ${Date.now()} +0000\n\n`),
             Buffer.from(`${this.message}\n`),
         ])
         const header = `commit ${commitBuffer.length}\0`;
